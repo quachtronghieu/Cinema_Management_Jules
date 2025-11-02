9504aed5e9e1cafc1100662ebbcf0b500f79fb76
Thungan0307 <nganndt.ce190661@gmail.com>
2025-11-02 15:43:19 +0700

    delete CustomerProfile(Service, Controller), update view profile, update profile, change password

--- src/main/java/vn/edu/fpt/cinemamanagement/controller/CustomerController.java
+++ src/main/java/vn/edu/fpt/cinemamanagement/controller/CustomerController.java
@@ -3,28 +3,69 @@ package vn.edu.fpt.cinemamanagement.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import vn.edu.fpt.cinemamanagement.entities.Customer;
+import vn.edu.fpt.cinemamanagement.services.CustomerProfileService;
 import vn.edu.fpt.cinemamanagement.services.CustomerService;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
-@RequestMapping(value = "/customer")
+@Controller()
+@RequestMapping("customer/profile")
 public class CustomerController {
-    private CustomerService customerService;
     @Autowired
-    public void setCustomerService(CustomerService customerService) {
-        this.customerService = customerService;
+    private CustomerService customerService;
+
+    @GetMapping("/my_profile")
+    public String viewProfile(Model model, Principal principal) {
+        if (principal != null) {
+            String username = principal.getName();
+            Customer customer = customerService.getCustomerByUsername(username);
+            model.addAttribute("customer", customer);
+        }
+        if (!model.containsAttribute("errors")) {
+            model.addAttribute("errors", new HashMap<String, String>());
+        }
+        return "customers/my_profile";
+    }
+
+    @GetMapping("/edit")
+    public String editProfileForm(Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+
+        String username = principal.getName();
+        Customer customer = customerService.getCustomerByUsername(username);
+        model.addAttribute("customer", customer); // trùng với th:object
+
+        if (!model.containsAttribute("errors")) {
+            model.addAttribute("errors", new HashMap<String, String>());
+        }
+
+        return "customers/update_profile"; // phải trùng với tên file Thymeleaf
+    }
+
+
+    @PostMapping("/update")
+    public String updateProfile(@ModelAttribute("customer") Customer customer, Model model) {
+        Map<String, String> errors = customerService.validateCustomer(customer);
+
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            model.addAttribute("customer", customer);
+            return "customers/update_profile";
+        }
+
+        customerService.save(customer);
+
+        return "redirect:/profile/my_profile";
     }
 
     @GetMapping("changePassword")
@@ -35,11 +76,11 @@ public class CustomerController {
 
     @PostMapping("/change")
     public String change(@RequestParam("currentPassword")  String currentPassword,
-                                 @RequestParam("newPassword") String newPassword,
-                                 @RequestParam("confirmPassword") String confirmPassword,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response,
-                                 Model model) {
+                         @RequestParam("newPassword") String newPassword,
+                         @RequestParam("confirmPassword") String confirmPassword,
+                         HttpServletRequest request,
+                         HttpServletResponse response,
+                         Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Map<String, String> errors = customerService.changePassword(username, currentPassword, newPassword, confirmPassword);
 
