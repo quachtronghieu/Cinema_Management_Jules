ab5370bdf4cc6d722de33dd9f83a1260b451f36c
Le Tran Tuong Vy <vyltt.ce190528@gmail.com>
2025-11-15 08:52:27 +0700

    Change payment sucess retun page

--- src/main/java/vn/edu/fpt/cinemamanagement/controller/PaymentController.java
+++ src/main/java/vn/edu/fpt/cinemamanagement/controller/PaymentController.java
@@ -57,7 +57,7 @@ public class PaymentController {
                 .orElseThrow(() -> new RuntimeException("Staff not found")));
 
         paymentRepository.save(payment);
-        String qrContent = "https://unquivering-latrice-semisentimental.ngrok-free.dev/payments/paymentsuccess?pay=" + paymentId;
+        String qrContent = " https://unquivering-latrice-semisentimental.ngrok-free.dev/payments/paymentsuccess?pay=" + paymentId;
 
         model.addAttribute("content", qrContent);
         return "/payment/QR_payment";
@@ -124,7 +124,7 @@ public class PaymentController {
         payment.setAmount(booking.getTotalAmount());
 
         paymentRepository.save(payment);
-        String qrContent = "https://overtensely-communal-greta.ngrok-free.dev/payments/paymentsuccess?pay=" + paymentId;
+        String qrContent = " https://unquivering-latrice-semisentimental.ngrok-free.dev/payments/paymentsuccess?pay=" + paymentId;
 
         model.addAttribute("content", qrContent);
         return "/booking/payment";
